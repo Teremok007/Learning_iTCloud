af1cce709c5138ffea458a077a0113d4e30b7734
Teremok007 <Nthtvjr007>
2018-10-27 03:38:14 +0300

    sync

--- Addition/ScanFolder/ConsoleApp5/Program.cs
+++ Addition/ScanFolder/ConsoleApp5/Program.cs
@@ -23,63 +23,110 @@ namespace ConsoleApp5
             Console.WriteLine($"Scaning directoory {RootDirectory}...");
 
             Stack<FolderItem> WorkFolders = new Stack<FolderItem>();
-            
-            using (FileStream reportFileName = new FileStream(StructDirFileName,FileMode.OpenOrCreate))
+            try
             {
-                using (StreamWriter sw = new StreamWriter(reportFileName))
+                using (FileStream reportFileName = new FileStream(StructDirFileName, FileMode.OpenOrCreate))
                 {
-                    sw.WriteLine(RootDirectory);
-                    FolderItem fl = new FolderItem(0, RootDirectory);
-                    WorkFolders.Push(fl);
-                    while (WorkFolders.Count != 0)
+                    using (StreamWriter sw = new StreamWriter(reportFileName))
                     {
-                        fl = WorkFolders.Pop();
-                        if (fl.ScanFiles())
+                        sw.WriteLine(RootDirectory);
+                        FolderItem fl = new FolderItem(0, RootDirectory);
+                        WorkFolders.Push(fl);
+                        while (WorkFolders.Count != 0)
                         {
-                            //buf.Add(fl.path.Replace(Path.GetDirectoryName(RootDirectory), ""));
-                            //buf.AddRange(fl.Files);
-                            sw.WriteLine(fl.path.Replace(Path.GetDirectoryName(RootDirectory), ""));
-                            foreach (var fileName in fl.files)
-                                sw.WriteLine(fileName);
-                        }
-                        foreach (FolderItem f in fl.SubFolders)
-                        {
-                            WorkFolders.Push(f);
+                            fl = WorkFolders.Pop();
+                            if (fl.ScanFiles())
+                            {
+                                sw.WriteLine(fl.path.Replace(Path.GetDirectoryName(RootDirectory), ""));
+                                foreach (var fileName in fl.files)
+                                    sw.WriteLine(fileName);
+                            }
+                            foreach (FolderItem f in fl.SubFolders)
+                            {
+                                WorkFolders.Push(f);
+                            }
                         }
                     }
-                 
                 }
             }
-            CompressingFile(StructDirFileName, CompressedFileName);
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
+            
+            CompressingFile(StructDirFileName, CompressedFileName);
+            MoveToDesktop(CompressedFileName);
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
         }
-        public static void CompressingFile(string src, string dst)
+
+        #region Compress data from src file and save it into dst compressed file
+        public static void CompressingFile(string src, string dst)        
         {
             if (!File.Exists(src))
             {
                 return;
             }
-
-            using (FileStream sourceFile = new FileStream(src, FileMode.Open))
+            try
             {
-                using (FileStream compressedFile = new FileStream(dst, FileMode.OpenOrCreate))
+                using (FileStream sourceFile = new FileStream(src, FileMode.Open))
                 {
-                    using (GZipStream gs = new GZipStream(compressedFile, CompressionMode.Compress))
+                    try
                     {
-                        sourceFile.CopyTo(gs);
+                        using (FileStream compressedFile = new FileStream(dst, FileMode.OpenOrCreate))
+                        {
+                            using (GZipStream gs = new GZipStream(compressedFile, CompressionMode.Compress))
+                            {
+                                sourceFile.CopyTo(gs);
+                            }
+                        }
                     }
-                }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine($"Невозможно получить доступ к файлу {dst}, возможно он используется другтм процессом.");
+                    }
+                }                
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine($"Невозможно получить доступ к файлу {src}, возможно он используется другтм процессом.");
             }
         }
-        
-        public static void MoveToDesktop(string  GZipFileName,string fileTo)
+        #endregion
+
+        // Move Compressed file te desktop
+        public static void MoveToDesktop(string  GZipFileName)
         {
+            string dstFileName = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + GZipFileName;
             if (!File.Exists(GZipFileName))
+            {               
                 return;
-            File.Move(GZipFileName, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + GZipFileName);                
+            }
+            if (File.Exists(dstFileName))
+            {
+                Console.WriteLine($"Файл уже {GZipFileName} существует в папке {Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\"}");
+                if (!ConfirmYN("Заменить ?"))
+                    return;
+            }
+
+            try
+            {
+                File.Move(GZipFileName, dstFileName);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+        public static bool ConfirmYN(string aMessage)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{aMessage} [Y/N](N):");
+            return Console.ReadKey().KeyChar.ToString().ToLower() == "y";            
+        }
+
     }
 
 }
